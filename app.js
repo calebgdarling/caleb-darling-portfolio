34bfe01e38620d19aa034883769a70b80925da3f
Caleb Darling <calebd@marcatopartners.com>
2023-10-09 14:50:32 -0500

    Added Contact component

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Contact from './components/Contact';
 
 const App = () => {
   return (
     <div>
       <h1>Hello, React!</h1>
+      <Contact />
     </div>
   );
 };
