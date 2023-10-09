a157955d5f5ac58222202bf84fe79080ecb93926
Caleb Darling <calebd@marcatopartners.com>
2023-10-09 15:21:07 -0500

    added app.js and index.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      {/* Other components and content */}
+    </div>
+  );
+};
+
+export default App;
