b286afb8ef6a335b53b977e34b30a3f8c0c41348
Caleb Darling <calebd@marcatopartners.com>
2023-10-10 16:02:34 -0500

    added routing to Github Pages home

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/caleb-darling-portfolio" element={<Home />} />
         <Route path="/skills" element={<SkillsPage />} />
         <Route path="/projects" element={<ProjectsPage />} />
       </Routes>
